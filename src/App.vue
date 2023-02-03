e91555bc62d5c511256e51b68d35285373a28b91
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-03 17:19:11 +0300

    009

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script>
 export default {
   data() {
-    return {
-      obj: {x: 1, y: 2, z: 3},
+
+  },
+  methods: {
+    show: function() {
+      alert('03.02.2023');
     }
   }
 }
 </script>
 <template>
-  <p>№1: obj.x + obj.y + obj.z = {{ obj.x + obj.y + obj.z }}</p><br>
+  {{ show() }}
 </template>
