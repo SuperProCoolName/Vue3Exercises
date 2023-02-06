565db4f4340fc43448421f48f4cccab741ad6ac4
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-06 16:34:08 +0300

    012

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,41 @@
 export default {
   data() {
     return {
-      num1: 1,
-      num2: 2,
+      date: 'if you see this then something went wrong'
     }
   },
   methods: {
 	show: function() {
-		alert(`this.num1 + this.num2 = ${this.num1 + this.num2}`);
+    const current = new Date(); 
+    const dayOfWeekNumber = current.getDay();
+    let dayOfWeek;
+    switch (dayOfWeekNumber) {
+      case 1:
+        dayOfWeek = "Monday";
+        break;
+      case 2:
+        dayOfWeek = "Tuesday";
+        break;
+      case 3:
+        dayOfWeek = "Wednesday";
+        break;
+      case 4:
+        dayOfWeek = "Thursday";
+        break;
+      case 5:
+        dayOfWeek = "Friday";
+        break;
+      case 6:
+        dayOfWeek = "Saturday";
+        break;
+      case 7:
+        dayOfWeek = "Sunday";
+        break; 
+      default:
+        dayOfWeek = "Something went wrong"
+        break;
+    }
+		alert(`Day of week by number = ${dayOfWeekNumber}. Day of week = ${dayOfWeek}`);
 	}
 }
 }
