19912e023f407e48a0babea6cb53a35d72b8db26
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-06 20:35:33 +0300

    16

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,17 @@
 export default {
   data() {
     return {
-      cost: 50,
-      amount: 3,
-      price: 'Press to compute the price',
+
     }
   },
   methods: {
-    computePrice: function() {
-      this.price = (this.cost * this.amount);
+    alyarm: function() {
+      alert(`This will only be shown once`);
     }
   }
 }
 </script>
 <template>
-  <h3>Enter number for cost:</h3>
-  <input type="number" v-model.number="cost" ref="costField">
-  <h3>Enter number for amount:</h3>
-  <input type="number" v-model.number="amount" ref="amountField">
-  <button @click="computePrice">{{ price }}</button>
+  <a href.prevent="https://github.com/SuperProCoolName/Vue3Exercises" target="_blank">Link</a><br>
+  <button @click.once="alyarm">Button</button>
 </template>
