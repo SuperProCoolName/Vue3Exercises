2aa11553b3c4c28435b43b018f20f586976168fa
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-06 20:15:17 +0300

    14

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,22 @@
 export default {
   data() {
     return {
-      date: 'if you see this then something went wrong'
+      text: 'type',
+      data: 'if you see this then something went wrong',
     }
   },
   methods: {
-	show: function() {
-    const number = this.$refs.inputField.value;
-		alert(`${number * number}`);
-	},
-  power_two: function() {
-    alert(`${2 * 2}`);
-  },
-  power_three: function() {
-    alert(`${3 * 3}`);  
+    changeToType: function() {
+      this.text = 'type';
+    },
+    changeToWrite: function() {
+      this.text = 'write';
+    }
   }
-}
 }
 </script>
 <template>
-  <h3>Enter number:</h3>
-  <input type="number" ref="inputField"><br>
-  <button @click="show()">Try it</button>
-
-  <button @click="power_two()">Two to the power of 2</button><br>
-  <button @click="power_three()">Three to the power of 2</button>
+  <h1>This text is written using something i don't know what to {{ text }} here</h1><br>
+  <button @click="changeToWrite">Change to "write"</button><br>
+  <button @click="changeToType">Change to "type"</button> 
 </template>
