e8a4cfa491d54e170290b480f8fb7c4a57d44727
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-09 19:26:19 +0300

    22

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,18 @@
 export default {
   data() {
     return {
-      visibility_p1: false,
-      visibility_p2: false,
-      visibility_p3: false,
+      isAdmin: true,
     }
   },
   methods: {
-      toggle_p1: function() {
-        this.visibility_p1 = !this.visibility_p1;
-      },
-      toggle_p2: function() {
-        this.visibility_p2 = !this.visibility_p2;
-      },
-      toggle_p3: function() {
-        this.visibility_p3 = !this.visibility_p3;
-      },
+    toggle: function() {
+      this.isAdmin = !this.isAdmin;
+    }
   }
 }
 </script>
 <template>
-  <button @click="toggle_p1">{{ visibility_p1 ? 'hide' : 'show' }}</button><br>
-  <p v-if="visibility_p1">text p1</p>
-  <button @click="toggle_p2">{{ visibility_p2 ? 'hide' : 'show' }}</button><br>
-  <p v-if="visibility_p2">text p2</p>
-  <button @click="toggle_p3">{{ visibility_p3 ? 'hide' : 'show' }}</button><br>
-  <p v-if="visibility_p3">text p3</p>
-  
+  <button @click="toggle">{{ isAdmin ? "give admin" : "take admin" }}</button><br>
+  <p v-if="isAdmin">the user is admin</p>
+  <p v-else>the user is NOT admin</p>
 </template>
