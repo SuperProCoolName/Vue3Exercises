e6da2a80c2bc6ff18c1b7257a0eebb39eff88fc1
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-09 20:25:28 +0300

    24

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,18 @@
 export default {
   data() {
     return {
-      day: "if you see this then something went wrong",
+      age: "if you see this then something went wrong",
     }
   },
   methods: {
-    dayOfTheWeek: function() {
-    this.day = new Date().getDay();
-  },
-}
+
+  }
 }
 </script>
 <template>
-  <button @click="dayOfTheWeek">Click to reveal paragraph</button><br>
-  <p v-if="this.day === 1">Monday</p>
-  <p v-if="this.day === 2">Tuesday</p>
-  <p v-if="this.day === 3">Wednesday</p>
-  <p v-if="this.day === 4">Thursday</p>
-  <p v-if="this.day === 5">Friday</p>
-  <p v-if="this.day === 6">Saturday</p>
-  <p v-if="this.day === 0">Sunday</p>
+  <h3>Enter age: </h3><br>
+  <input type="number" v-model.number="age">
+  <p v-if="age < 18">Подросток</p>
+  <p v-else-if="age < 25">Молодой человек</p>
+  <p v-else-if="age >= 25">Мужчина</p>
 </template>
