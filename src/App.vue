a30571a33036bbc73e147cb1859417781206ac99
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-10 22:27:40 +0300

    30

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,7 @@
 export default {
   data() {
     return {
-      data: {
-      user1: '100$',
-      user2: '200$',
-      user3: '300$',
-      }
+      items: [1, 2, 3],
     }
   },
   methods: {
@@ -15,20 +11,14 @@ export default {
 }
 </script>
 <template>
-  <ul>
-    Задание 1
-    <li v-for="value in data">{{ value }}</li>
-  </ul>
-  <ul>
-    Задание 2
-    <li v-for="(value, key) in data">{{ key }} - {{ value }}</li>
-  </ul>
-  <ul>
-    Задание 3
-    <li v-for="(value, key, index) in data">{{ key }} - {{ value }} - {{ index }}</li>
-  </ul>
-  <ul>
-    Задание 4
-    <li v-for="(value, key, index) in data">{{ key }} - {{ value }} - {{ index + 1 }}</li>
-  </ul>
+  	<template v-for="elem in items">
+		<p>&nbsp;{{ elem }}&nbsp;</p>
+		<hr>
+    </template>
+    <ul v-for="elem in items">
+      <li>
+        <p>&nbsp;{{ elem }}&nbsp;</p>
+      </li>
+      <hr>
+    </ul>
 </template>
