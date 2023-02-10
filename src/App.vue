e5ff8d0bb008927692e35717856b6baacf43509d
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-10 16:32:42 +0300

    28

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return {
-      items: [1, 2, 3, 4, 5],
+      arr: ['x', 'y', 'z'],
     }
   },
   methods: {
@@ -11,22 +11,5 @@ export default {
 }
 </script>
 <template>
-    <div class="red-box"><div v-for="elem in items">{{ elem }} &nbsp;</div></div>
-    <div class="red-box"><br><div v-for="elem in items">{{ elem ** 2 }} &nbsp;</div></div>
-    <div class="red-box right"><div v-for="elem in items"><ul><li>{{ elem }}</li></ul>&nbsp;</div></div>
+  <p v-for="(elem, key) in arr">{{ key }}:&nbsp {{ elem }}</p>
 </template>
-<style>
-div {
-  display: flex;
-}
-.red-box {
-  border: 5px solid red;
-  margin: 10px;
-  padding: 2px;
-}
-div.right {
-  margin-top: 10px;
-  display: flex;
-  flex-direction: column;
-}
-</style>
