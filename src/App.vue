f2fa99eac0a7b28765cbb2ecba2e282261838c4c
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-10 12:35:23 +0300

    27

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,31 @@
 export default {
   data() {
     return {
-      visibility: false,
+      items: [1, 2, 3, 4, 5],
     }
   },
   methods: {
-    toggle: function() {
-      this.visibility = !this.visibility;
+
     }
-  }
 }
 </script>
 <template>
-  <p v-show="visibility">Paragraph 1</p><br>
-  <button @click="toggle">Toggle</button>
+    <div class="red-box"><div v-for="elem in items">{{ elem }} &nbsp;</div></div>
+    <div class="red-box"><br><div v-for="elem in items">{{ elem ** 2 }} &nbsp;</div></div>
+    <div class="red-box right"><div v-for="elem in items"><ul><li>{{ elem }}</li></ul>&nbsp;</div></div>
 </template>
+<style>
+div {
+  display: flex;
+}
+.red-box {
+  border: 5px solid red;
+  margin: 10px;
+  padding: 2px;
+}
+div.right {
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+}
+</style>
