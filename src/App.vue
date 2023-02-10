7e8691702516c2003dfd6e052dce3f72f60d57da
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-10 12:11:13 +0300

    25

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
 export default {
   data() {
     return {
-      age: "if you see this then something went wrong",
+      visibility: true,
     }
   },
   methods: {
-
+    toggle: function() {
+      this.visibility = !this.visibility;
+    }
   }
 }
 </script>
 <template>
-  <h3>Enter age: </h3><br>
-  <input type="number" v-model.number="age">
-  <p v-if="age < 18">Подросток</p>
-  <p v-else-if="age < 25">Молодой человек</p>
-  <p v-else-if="age >= 25">Мужчина</p>
+  <div v-if="visibility">
+  <p>Paragraph 1</p>
+  <p>Paragraph 2</p>
+  <p>Paragraph 3</p>
+  </div><br>
+  <button @click="toggle">Toggle</button>
 </template>
