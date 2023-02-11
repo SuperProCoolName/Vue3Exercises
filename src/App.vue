2129f380cacee0998c142a13160f319573f9e09d
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-11 12:50:51 +0300

    35

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,39 @@
 export default {
   data() {
     return {
-      products: [
-			{
-				id: 1,
-				name: 'product1',
-			},
-			{
-				id: 2,
-				name: 'product2',
-			},
-			{
-				id: 3,
-				name: 'product3',
-			},
-		]
+      arr: ['a', 'c', 'd'],
     }
   },
   methods: {
-
-    }
+    add: function() {
+		  this.arr.push('b');
+  },
+    delFirst: function() {
+      this.arr.shift();
+  },
+    delLast: function() {
+      this.arr.pop();
+  },
+    delSecondToLast: function() {
+      this.arr.splice(-2, 1);
+  },
+    sortArr: function() {
+      this.arr.sort();
+  },
+    reverseOrder: function() {
+      this.arr.reverse();
+  }
+}
 }
 </script>
 <template>
-  <ul v-for="smh in products" :key="smh.id">
-    <li>{{ smh.name }}</li>
-  </ul>
+  <button @click.once="add">Задание 1</button>
+  <button @click.once="delFirst">Задание 2</button>
+  <button @click.once="delLast">Задание 3</button>
+  <button @click.once="delSecondToLast">Задание 4</button>
+  <button @click.once="sortArr">Задание 5</button>
+  <button @click.once="reverseOrder">Задание 6</button>
+  <ul v-for="elem in arr">
+    <li>{{ elem }}</li>
+  </ul><br>
 </template>
