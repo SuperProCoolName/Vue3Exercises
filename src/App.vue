5f2fe00c08f57be27d8560513763e9d85e424145
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-15 19:24:58 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,18 @@ export default {
   data() {
     return {
       isAdmin: true,
-    }
+    };
   },
   methods: {
-    toggle: function() {
+    toggle: function () {
       this.isAdmin = !this.isAdmin;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 <template>
-  <button @click="toggle">{{ isAdmin ? "give admin" : "take admin" }}</button><br>
-  <p v-if="isAdmin">the user is admin</p>
-  <p v-else>the user is NOT admin</p>
+  <button @click="toggle">{{ isAdmin ? "give admin" : "take admin" }}</button
+  ><br />
+  <p v-if="isAdmin">the user is NOT admin</p>
+  <p v-else>the user is admin</p>
 </template>
