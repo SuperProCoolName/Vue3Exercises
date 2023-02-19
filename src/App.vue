a5df43081fce3d03075b36d37a8eedec2803cb77
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-19 16:15:51 +0300

    46

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,14 @@
 export default {
   data() {
     return {
-      text: "",
-      text_button: "",
-      text_array: "",
+      checked: true,
     };
   },
-  methods: {
-    split_text: function () {
-      this.text_array = this.text_button.split(" ");
-    },
-  },
+  methods: {},
 };
 </script>
 <template>
-  <textarea v-model="text"></textarea>
+  <input type="checkbox" v-model="checked" />
   &nbsp;
-  <p>{{ text }}</p>
-  &nbsp;
-  <textarea v-model="text_button"></textarea>
-  &nbsp;
-  <button @click="split_text">Array</button>
-  &nbsp;
-  <ul v-for="elem in text_array">
-    <li :key="key">
-      {{ elem }}
-    </li>
-  </ul>
+  <p v-if="checked">{{ checked }}</p>
 </template>
