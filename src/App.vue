ed52471a59655e0c0e155db538e313ffe74434fb
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-19 18:26:09 +0300

    53

--- src/App.vue
+++ src/App.vue
@@ -2,52 +2,27 @@
 export default {
   data() {
     return {
-      var_1: "Задание 1",
-      isCtrlPressed: false,
-      var_2: "you pressed control + link",
+      newItem: "",
+      items: ["a", "b", "c", "d", "e"],
     };
   },
   methods: {
-    lag: function () {
-      this.var_1 = this.$refs.textField.value;
+    addItemEnd: function () {
+      this.items.push(this.newItem);
     },
-    leg: function (event) {
-      if (event.ctrlKey) {
-        this.isCtrlPressed = true;
-      }
-    },
-    handleLeftClick: function (event) {
-      if (!event.ctrlKey) {
-        this.var_2 = "left";
-      }
-    },
-    handleRightClick: function (event) {
-      if (!event.ctrlKey) {
-        this.var_2 = "right";
-      }
-    },
-    handleMiddleClick: function (event) {
-      if (!event.ctrlKey) {
-        this.var_2 = "middle";
-      }
+    addItemBegin: function () {
+      this.items.unshift(this.newItem);
     },
   },
 };
 </script>
 <template>
-  <input type="text" @keyup.enter="lag" ref="textField" />
-  &nbsp;
-  <p>{{ var_1 }}</p>
-  &nbsp;
-  <a
-    href="https://www.youtube.com/watch?v=R05dgTwTN6I"
-    @click="leg"
-    @click.left="handleLeftClick"
-    @click.right="handleRightClick"
-    @click.middle="handleMiddleClick"
-    target="_blank"
-    >Задание 2 и 3</a
-  >
-  &nbsp;
-  <p v-if="isCtrlPressed">{{ var_2 }}</p>
+  <input v-model="newItem" />
+  <button @click="addItemEnd">add to the end</button>
+  <button @click="addItemBegin">add to the beginning</button>
+  <ul v-for="(item, index) in items" :key="index">
+    <li>
+      {{ item }}
+    </li>
+  </ul>
 </template>
