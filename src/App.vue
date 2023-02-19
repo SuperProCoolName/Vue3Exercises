04e6a11a13630bd79f6161e4a2d9230e9dedfd1f
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-19 16:22:18 +0300

    47

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,25 @@
 export default {
   data() {
     return {
-      checked: true,
+      languages: [],
     };
   },
   methods: {},
 };
 </script>
 <template>
-  <input type="checkbox" v-model="checked" />
+  <input type="checkbox" v-model="languages" value="english" />
   &nbsp;
-  <p v-if="checked">{{ checked }}</p>
+  <p>English</p>
+  &nbsp;
+  <input type="checkbox" v-model="languages" value="russian" />
+  &nbsp;
+  <p>Russian</p>
+  <input type="checkbox" v-model="languages" value="javascript" />
+  &nbsp;
+  <p>JavaScript</p>
+  &nbsp;
+  <ul v-for="elem in languages">
+    <li>{{ elem }}</li>
+  </ul>
 </template>
