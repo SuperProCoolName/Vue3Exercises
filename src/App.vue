01ddb683d27097dd3f9f8663701382a7b1e30383
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-19 16:44:50 +0300

    49

--- src/App.vue
+++ src/App.vue
@@ -2,64 +2,21 @@
 export default {
   data() {
     return {
-      choice: "",
-      english: true,
-      russian: false,
-      binary: false,
+      selected: "Выберите город",
     };
   },
-  methods: {
-    eng: function () {
-      this.english = true;
-      this.russian = false;
-      this.binary = false;
-    },
-    rus: function () {
-      this.english = false;
-      this.russian = true;
-      this.binary = false;
-    },
-    bin: function () {
-      this.english = false;
-      this.russian = false;
-      this.binary = true;
-    },
-  },
+  methods: {},
 };
 </script>
 <template>
-  <p v-if="english">What is your mother tongue?</p>
-  <p v-if="russian">Какой ваш родной язык?</p>
-  <p v-if="binary">
-    01110111 01101000 01100001 01110100 00100000 01101001 01110011 00100000
-    01111001 01101111 01110101 01110010 00100000 01101101 01101111 01110100
-    01101000 01100101 01110010 00100000 01110100 01101111 01101110 01100111
-    01110101 01100101 00111111
-  </p>
+  <select v-model="selected">
+    <option>Амстердам</option>
+    <option>Лондон</option>
+    <option>Нью-Йорк</option>
+    <option>Токио</option>
+    <option>Новокузнецк</option>
+    <option>Париж</option>
+  </select>
   &nbsp;
-  <input
-    name="radio"
-    type="radio"
-    v-model="choice"
-    value="English"
-    @click="eng"
-  />
-  &nbsp;
-  <input
-    name="radio"
-    type="radio"
-    v-model="choice"
-    value="Русский"
-    @click="rus"
-  />
-  &nbsp;
-  <input
-    name="radio"
-    type="radio"
-    v-model="choice"
-    value="01100010 01101001 01101110 01100001 01110010 01111001"
-    @click="bin"
-  />
-  &nbsp;
-  <p>{{ choice }}</p>
+  <p>{{ selected }}</p>
 </template>
