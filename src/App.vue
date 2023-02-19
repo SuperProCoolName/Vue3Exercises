e3857c6a09515e77d62a8baae0da5085ad5ae3dc
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-19 16:40:34 +0300

    48

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,64 @@
 export default {
   data() {
     return {
-      languages: [],
+      choice: "",
+      english: true,
+      russian: false,
+      binary: false,
     };
   },
-  methods: {},
+  methods: {
+    eng: function () {
+      this.english = true;
+      this.russian = false;
+      this.binary = false;
+    },
+    rus: function () {
+      this.english = false;
+      this.russian = true;
+      this.binary = false;
+    },
+    bin: function () {
+      this.english = false;
+      this.russian = false;
+      this.binary = true;
+    },
+  },
 };
 </script>
 <template>
-  <input type="checkbox" v-model="languages" value="english" />
+  <p v-if="english">What is your mother tongue?</p>
+  <p v-if="russian">Какой ваш родной язык?</p>
+  <p v-if="binary">
+    01110111 01101000 01100001 01110100 00100000 01101001 01110011 00100000
+    01111001 01101111 01110101 01110010 00100000 01101101 01101111 01110100
+    01101000 01100101 01110010 00100000 01110100 01101111 01101110 01100111
+    01110101 01100101 00111111
+  </p>
   &nbsp;
-  <p>English</p>
+  <input
+    name="radio"
+    type="radio"
+    v-model="choice"
+    value="English"
+    @click="eng"
+  />
   &nbsp;
-  <input type="checkbox" v-model="languages" value="russian" />
+  <input
+    name="radio"
+    type="radio"
+    v-model="choice"
+    value="Русский"
+    @click="rus"
+  />
   &nbsp;
-  <p>Russian</p>
-  <input type="checkbox" v-model="languages" value="javascript" />
+  <input
+    name="radio"
+    type="radio"
+    v-model="choice"
+    value="01100010 01101001 01101110 01100001 01110010 01111001"
+    @click="bin"
+  />
   &nbsp;
-  <p>JavaScript</p>
-  &nbsp;
-  <ul v-for="elem in languages">
-    <li>{{ elem }}</li>
-  </ul>
+  <p>{{ choice }}</p>
 </template>
