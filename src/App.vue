cc153c7d43671aa8d1d4ad6dd44fbccb26d3df06
SuperProCoolName <minzatov.2004@mail.ru>
2023-03-01 17:13:38 +0300

    64

--- src/App.vue
+++ src/App.vue
@@ -4,38 +4,7 @@ export default {
   components: {
     Employee,
   },
-  data() {
-    return {
-      users: [
-        {
-          id: 1,
-          name: "name1",
-          salary: 100,
-          age: 30,
-        },
-        {
-          id: 2,
-          name: "name2",
-          salary: 200,
-          age: 40,
-        },
-        {
-          id: 3,
-          name: "name3",
-          salary: 300,
-          age: 50,
-        },
-      ],
-    };
-  },
+  data() {},
 };
 </script>
-<template>
-  <Employee
-    v-for="user in users"
-    :name="user.name"
-    :salary="user.salary"
-    :age="user.age"
-    :key="user.id"
-  />
-</template>
+<template></template>
